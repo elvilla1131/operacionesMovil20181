4a4314185129b792e3ea3576291dd1596bf12ea7
Jose <joseeduardovillanuevaferrer@gmail.com>
2018-02-26 20:23:27 -0500

    funcionalidad sumar terminada sin validaciones

---
+++ app/src/main/java/com/elvilla/operacionesmoviles20181/Metodos.java
@@ -0,0 +1,14 @@
+package com.elvilla.operacionesmoviles20181;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar (double n1, double n2) {
+
+        return n1+n2;
+    }
+
+}
--- app/src/main/java/com/elvilla/operacionesmoviles20181/Principal.java
+++ app/src/main/java/com/elvilla/operacionesmoviles20181/Principal.java
@@ -2,12 +2,34 @@ package com.elvilla.operacionesmoviles20181;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
-
+    private EditText n1, n2;
+    private TextView res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        n1 = findViewById(R.id.txtNumeroUno);
+        n2 = findViewById(R.id.txtNumeroDos);
+        res = findViewById(R.id.lblResultado);
+
+
+
+
+
     }
+    public void calcular(View v){
+        double num1, num2,resultado;
+        num1=Double.parseDouble(n1.getText().toString());
+        num2=Double.parseDouble(n2.getText().toString());
+
+        resultado = Metodos.sumar(num1,num2);
+        res.setText(""+resultado);
+    }
+
 }
