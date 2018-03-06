7f3fc3cc955354f3e8fae37222929cf05cd051a7
Jose <joseeduardovillanuevaferrer@gmail.com>
2018-03-05 20:25:21 -0500

    operaciones sin testing

--- app/src/main/java/com/elvilla/operacionesmoviles20181/Principal.java
+++ app/src/main/java/com/elvilla/operacionesmoviles20181/Principal.java
@@ -4,13 +4,20 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
     private EditText n1, n2;
     private TextView res;
     private Resources recursos;
+    private Spinner operaciones;
+    private String op[];
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,29 +27,90 @@ public class Principal extends AppCompatActivity {
         n1 = findViewById(R.id.txtNumeroUno);
         n2 = findViewById(R.id.txtNumeroDos);
         res = findViewById(R.id.lblResultado);
+        operaciones = findViewById(R.id.cmbOperacion);
         recursos=this.getResources();
+        op = recursos.getStringArray(R.array.operaciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter( this,android.R.layout.simple_spinner_item,op);
+        operaciones.setAdapter(adapter);
+        boton = findViewById(R.id.btnOperacion);
 
+        operaciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        boton.setText(recursos.getString(R.string.sumar));
+                        break;
+                    case 1:
+                        boton.setText(recursos.getString(R.string.restar));
+                        break;
+                    case 2:
+                        boton.setText(recursos.getString(R.string.multiplicar));
+                        break;
+                    case 3:
+                        boton.setText(recursos.getString(R.string.dividir));
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
     }
-    public void calcular(View v) {
-        double num1, num2, resultado;
+
+    public boolean validar (){
+        int opcion = operaciones.getSelectedItemPosition();
+
 
         if (n1.getText().toString().isEmpty()) {
             n1.requestFocus();
             n1.setError(recursos.getString(R.string.error_numero_uno));
-        } else if (n2.getText().toString().isEmpty()) {
+            return false;
+        }
+        if (n2.getText().toString().isEmpty()) {
             n2.requestFocus();
             n2.setError(recursos.getString(R.string.error_numero_dos));
-        } else {
+            return false;
+        }
+        if (opcion==3 && Double.parseDouble(n2.getText().toString())==0){
+            n2.requestFocus();
+            n2.setError(recursos.getString(R.string.error_numero_tres));
+            return false;
+        }
+        return true;
+    }
 
+    public void calcular(View v) {
+        double num1, num2, resultado=0;
+        int opcion;
+        res.setText("");
+        if (validar()){
 
+            opcion = operaciones.getSelectedItemPosition();
             num1 = Double.parseDouble(n1.getText().toString());
             num2 = Double.parseDouble(n2.getText().toString());
+            switch (opcion){
+                case 0:
+                    resultado = Metodos.sumar(num1,num2);
+                    break;
+                case 1:
+                    resultado = Metodos.restar(num1,num2);
+                    break;
+                case 2:
+                    resultado = Metodos.multiplicar(num1,num2);
+                    break;
+                case 3:
+                    resultado = Metodos.dividir(num1,num2);
+                    break;
+            }
 
-            resultado = Metodos.sumar(num1, num2);
-            res.setText("" + resultado);
+            res.setText(""+String.format("%.2f",resultado));
+
         }
     }
 
@@ -51,5 +119,6 @@ public class Principal extends AppCompatActivity {
         n1.setText("");
         n2.setText("");
         n1.requestFocus();
+        operaciones.setSelection(0);
     }
 }
